f00fc2d9de0056fa6ee9229ce698aad5a8700176
Jasurbek <jasurbekkurganbayev@gmail.com>
2025-05-29 11:23:06 +0900

    ...

--- app/src/main/java/com/jsoft/adderproduct/MainActivity.kt
+++ app/src/main/java/com/jsoft/adderproduct/MainActivity.kt
@@ -57,7 +57,6 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     val selectedColors = mutableListOf<Int>()
     var selectedImages = mutableListOf<Uri>()
